d52671b52881fc7dd02796518e7abebccf970f78
SaeHie Park <saehie.park@gmail.com>
2025-01-21 10:53:34 +0900

    do ci test with tools module

---
+++ tools/ci-test-project/src/main.cpp
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+  (void)argc;
+  (void)argv;
+  printf("Hello World!\r\n");
+
+  return 0;
+}
---
+++ tools/ci-test-project/src/main.test.cpp
@@ -0,0 +1,7 @@
+#include <gtest/gtest.h>
+
+TEST(CI_TEST, Hello)
+{
+  // simple test
+  EXPECT_EQ(10, 2 * 5);
+}
